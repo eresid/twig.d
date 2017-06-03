494e47418b470ea736d9807888585ddf440f2ced
Eugene <eresid@gmail.com>
2017-06-03 14:05:29 +0300

    experiments with string concatenation

--- source/app.d
+++ source/app.d
@@ -23,7 +23,9 @@ void main()
 
     Data data = Data();
     data.title = "Awesome Twig.d";
-    write(tempMethod(data));
+    //write(tempMethod(data));
+
+    testFormat(true);
 }
 
 private void testImport() {
@@ -42,4 +44,48 @@ private string tempMethod(Data data) {
 
     str ~= "\n";
     return str;
+}
+
+private void testFormat(bool isFormatter) {
+    import std.array : appender;
+    import std.format : formattedWrite;
+    import std.conv : to;
+
+    if (isFormatter) {
+        //  50 - 0m1.501s 100k
+        // 100 - 0m2.846s 100k
+        // 200 - 0m5.615s 100k
+        // 200 - 0m3.924s 100k, release
+        // 200 - 0m2.753s 100k, -b=release --compiler=ldc2
+        // 500 - 0m6.793s 50k
+        // 500 - 0m3.271s 50k, -b=release --compiler=ldc2
+        for (int i=0; i<100_000; i++) {
+            auto writer = appender!string();
+
+            for (int j=0; j<200; j++) {
+                writer.put(to!string(42));
+                writer.put(" is the ultimate ");
+                writer.put("answer");
+                writer.put(".");
+            }
+
+            //writeln(writer.data); // "42 is the ultimate answer."
+        }
+    } else {
+        //  50 - 0m1.746s 100k
+        // 100 - 0m3.385s 100k
+        // 200 - 0m6.409s 100k
+        // 200 - 0m5.295s 100k, -b=release
+        // 200 - 0m4.728s 100k, -b=release --compiler=ldc2
+        // 500 - 0m7.846s 50k
+        // 500 - 0m5.487s 50k, -b=release --compiler=ldc2
+        for (int i=0; i<100_000; i++) {
+            auto str = "";
+
+            for (int j=0; j<200; j++) {
+                str ~= to!string(42) ~ " is the ultimate " ~ "answer" ~ ".";
+            }
+            //writeln(str);
+        }
+    }
 }
